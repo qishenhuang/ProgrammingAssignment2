abd1cfc3ab2417f84bd261ff33ecfe9dc08dc066
qishenhuang <qishenhuang@yahoo.com>
2015-04-17 14:22:32 -0400

    Added comments
    Added comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,7 @@
-## Put comments here that give an overall description of what your
+## avoid repeatign same work of finding inverse of a square matrix
 ## functions do
 
-## Write a short comment describing this function
+## make an object that holds basic operations and data
 
 makeCacheMatrix <- function(x = matrix()) {
   mInverse <- NULL
@@ -19,29 +19,38 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## smartly find or compute the inverse. It stores the result the first time.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
         
+  ## check if it is a mtrix
   if (!is.matrix(x))
     return(NA)
-  
+    
+  ## check if it is a SQUARE matrix
   if (!(nrow(x) == ncol(x)))
     return(NA)
   
+  ## check if the solution exists.  If yes, return it.
   mInverse <- x$getInverse()
-  if(!is.null(mInverse)) { 
+  if(!is.null(mInverse)) {   
     message("getting cached data")
     return(mInverse)
   }
   
+  ## if no existing solution, 
   data <- x$get
+  
+  ## make sure the inverse exists by checking its determinant
   d <- det(data)
   if(d == 0)
     return(NA)
   
+  ## compute inverse and store it
   mInverse <- solve(data)
   x$setInverse(mInverse)
+  
+  ## return result
   mInverse
 }
